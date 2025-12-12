d5adf215df97dfc0fef64a53fcc8d2a72e93f2e3
Nileena <nileena.ps123@gmail.com>
2025-12-13 04:29:24 +0530

    type predicate challenge

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,54 +1,11 @@
-type Student = {
-  name: string;
-  study: () => void;
-};
+function getFirstElement(array:(string|number)[]){
+    return array[0];
 
-type User = {
-  name: string;
-  login: () => void;
-};
-
-type Person = Student | User;
-
-const randomPerson = (): Person => {
-  return Math.random() > 0.5
-    ? { name: 'john', study: () => console.log('Studying') }
-    : { name: 'mary', login: () => console.log('Logging in') };
-};
-
-const person = randomPerson();
-
-let kathy:Person;
-let joe:Person;
-
-kathy={
-    name:'Kathy',
-    login:()=>{
-        console.log("Kathy logged in");
-    }
-};
-
-joe={
-    name:'Joe',
-    study:function(){
-        console.log('Joe studying')
-    }
-
-};
-
-function isStudent (person:Person):person is Student{
-    
- return 'study' in person;
- //return (person as Student).study!==undefined;
 }
 
-if(isStudent(person)){
-  person.study();
-}
-else{
-  person.login();
-}
+const nums=[1,2,3];
 
-isStudent(person);
-//console.log(isStudent(joe));
+getFirstElement(nums);
 
+const strs=['abc','sdff','jjjj'];
+getFirstElement(strs);
